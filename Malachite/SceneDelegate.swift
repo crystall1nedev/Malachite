bb8c143a3879f8d6711b44dfcea01832cecf5fc1
Eva Luna <me@crystall1ne.dev>
2023-11-26 00:53:16 -0500

    Add the project!
    This project is really straightforward and I'm actually already somewhat close to being done with it. I just need to get the buttons working properly, and smooth out transitions/add loading screens, etc...

---
+++ Malachite/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  Malachite
+//
+//  Created by Eva Isabella Luna on 11/25/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let rootVC = MalachiteView()
+        window?.rootViewController = rootVC
+        window?.makeKeyAndVisible()
+    }
+}
