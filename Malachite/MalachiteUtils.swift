5d4bd491626193cbcb385b55ddbeb32147f7e9aa
Eva Luna <me@crystall1ne.dev>
2023-12-11 17:53:02 -0500

    Continue the refactor, moving the functions of the buttons
    Daaaaaaamn this code is clean. I also moved the camera device init to viewDidLoad so it doesn't fire multiple times anymore!

--- Malachite/MalachiteUtils.swift
+++ Malachite/MalachiteUtils.swift
@@ -7,12 +7,14 @@
 
 import AVFoundation
 import Foundation
+import Photos
 import UIKit
 
 public class MalachiteClassesObject : NSObject {
-    public let versions = MalachiteVersion()
-    public let haptics  = MalachiteHapticUtils()
-    public let views    = MalachiteViewUtils()
+    public let versions  = MalachiteVersion()
+    public let haptics   = MalachiteHapticUtils()
+    public let views     = MalachiteViewUtils()
+    public let function  = MalachiteFuncUtils()
 }
 
 public class MalachiteVersion : NSObject {
@@ -63,5 +65,133 @@ public class MalachiteViewUtils : NSObject {
         blurView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         blurView.isUserInteractionEnabled = false
         return blurView
+    }
+}
+
+public class MalachiteFuncUtils : NSObject {
+    public func zoom(sender pinch: UIPinchGestureRecognizer, captureDevice device: inout AVCaptureDevice, lastZoomFactor zoomFactor: inout CGFloat, hapticClass haptic: MalachiteHapticUtils) {
+        func minMaxZoom(_ factor: CGFloat) -> CGFloat {
+            return min(min(max(factor, 1.0), 5.0), device.activeFormat.videoMaxZoomFactor)
+        }
+        
+        func update(scale factor: CGFloat) {
+            do {
+                try device.lockForConfiguration()
+                defer { device.unlockForConfiguration() }
+                device.videoZoomFactor = factor
+                NSLog("[Pinch to Zoom] Changed zoom factor")
+            } catch {
+                NSLog("[Pinch to Zoom] Error changing video zoom factor: %@", error.localizedDescription)
+            }
+        }
+        
+        let newScaleFactor = minMaxZoom(pinch.scale * zoomFactor)
+        
+        switch pinch.state {
+        case .began:
+            haptic.triggerMediumHaptic()
+            fallthrough
+        case .changed: update(scale: newScaleFactor)
+        case .ended:
+            zoomFactor = minMaxZoom(newScaleFactor)
+            update(scale: zoomFactor)
+            haptic.triggerMediumHaptic()
+        default: break
+        }
+    }
+    
+    
+    
+    public func autofocus(sender: UILongPressGestureRecognizer, captureDevice device: inout AVCaptureDevice, viewForScale view: UIView, hapticClass haptic: MalachiteHapticUtils) {
+        let focusPoint = sender.location(in: view)
+        let focusScaledPointX = focusPoint.x / view.frame.size.width
+        let focusScaledPointY = focusPoint.y / view.frame.size.height
+        if device.isFocusModeSupported(.autoFocus) && device.isFocusPointOfInterestSupported {
+            do {
+                try device.lockForConfiguration()
+            } catch {
+                print("[Tap to Zoom] Couldn't lock device for configuration: %@", error.localizedDescription)
+                return
+            }
+            
+            device.focusMode = .autoFocus
+            device.focusPointOfInterest = CGPointMake(focusScaledPointX, focusScaledPointY)
+            haptic.triggerNotificationHaptic(type: .success)
+            NSLog("[Tap to Focus] Changed focus area")
+            device.unlockForConfiguration()
+        }
+    }
+    
+    public func toggleFlash(captureDevice device: inout AVCaptureDevice, flashlightButton button: inout UIButton) {
+        if device.hasTorch {
+            var buttonImage = UIImage()
+            do {
+                try device.lockForConfiguration()
+                if (device.torchMode == AVCaptureDevice.TorchMode.on) {
+                    NSLog("[Flashlight] Flash is already on, turning off")
+                    device.torchMode = AVCaptureDevice.TorchMode.off
+                    buttonImage = (UIImage(systemName: "flashlight.off.fill")?.withRenderingMode(.alwaysTemplate))!
+                } else {
+                    do {
+                        NSLog("[Flashlight] Flash is off, turning on")
+                        try device.setTorchModeOn(level: 1.0)
+                        buttonImage = (UIImage(systemName: "flashlight.on.fill")?.withRenderingMode(.alwaysTemplate))!
+                    } catch {
+                        print(error)
+                        buttonImage = (UIImage(systemName: "flashlight.off.fill")?.withRenderingMode(.alwaysTemplate))!
+                    }
+                }
+                button.setImage(buttonImage, for: .normal)
+                device.unlockForConfiguration()
+            } catch {
+                print(error)
+                buttonImage = (UIImage(systemName: "flashlight.on.fill")?.withRenderingMode(.alwaysTemplate))!
+            }
+        }
+    }
+    
+    public func switchInput(session: inout AVCaptureSession, uwDevice: inout AVCaptureDevice?, waDevice: inout AVCaptureDevice, device: inout AVCaptureDevice?, input: inout AVCaptureDeviceInput?, button: inout UIButton, waInUse: inout Bool, firstRun: inout Bool){
+        button.isUserInteractionEnabled = false
+        NSLog("[Camera Input] Getting ready to configure session")
+        session.beginConfiguration()
+        
+        if !firstRun {
+            NSLog("[Camera Input] Removing currently active camera input")
+            session.removeInput(input!)
+        } else {
+            firstRun = false
+        }
+        
+        if waInUse == true && uwDevice != nil {
+            NSLog("[Camera Input] builtInUltraWideCamera is available, selecting as device")
+            device = uwDevice!
+            waInUse = false
+        } else {
+            NSLog("[Camera Input] builtInWideAngle is available, selecting as device")
+            device = waDevice
+            waInUse = true
+            
+        }
+        
+        NSLog("[Camera Input] Attempting to attach device input to session")
+        do { input = try AVCaptureDeviceInput(device: device!) }
+        catch {
+            print(error)
+        }
+        
+        NSLog("[Camera Input] Attached input, finishing configuration")
+        session.addInput(input!)
+        session.commitConfiguration()
+        button.isUserInteractionEnabled = true
+    }
+    
+    @objc public func captureImage(output photoOutput: AVCapturePhotoOutput, viewForBounds view: UIView, captureDelegate delegate: AVCapturePhotoCaptureDelegate) -> AVCapturePhotoOutput {
+        let photoSettings = AVCapturePhotoSettings()
+        if let photoPreviewType = photoSettings.availablePreviewPhotoPixelFormatTypes.first {
+            photoSettings.previewPhotoFormat = [kCVPixelBufferPixelFormatTypeKey as String: photoPreviewType]
+            photoOutput.capturePhoto(with: photoSettings, delegate: delegate)
+        }
+        
+        return photoOutput
     }
 }
