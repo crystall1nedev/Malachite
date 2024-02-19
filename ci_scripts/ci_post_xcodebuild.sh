149850d5d3deb2120baab5c7e15bb5fd87420f46
Eva Luna <me@crystall1ne.dev>
2024-02-18 21:23:47 -0500

    Remove the CAReplicatorLayer and add a Done button to the Settings View
    The CAReplicatorLayer can't do what I want to get done, which is a blur for the black background. It's a little but messy with it in, so I'm just removing it.
    I also added a Done button to the About View, at Stella's request.

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -12,6 +12,3 @@ if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
   echo "Automatic build - Last three commits:" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
   git fetch --deepen 3 && git log -3 --pretty=format:"%h by %an (%as): %s%n" >> $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
 fi
-
-defaults read com.apple.dt.Xcode >> /tmp/defaults_xcode.txt
-defaults read com.apple.CoreSimulator >> /tmp/defaults_coresim.txt
