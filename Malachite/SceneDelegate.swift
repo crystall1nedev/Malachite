cf1f48ee41f1e95057ee1e3d114a741ca367b386
Eva Luna <me@crystall1ne.dev>
2024-10-31 21:30:49 -0600

    This can be streamlined a little bit
    Still deciding on whether or not I want to rewrite to SwiftUI

--- Malachite/SceneDelegate.swift
+++ Malachite/SceneDelegate.swift
@@ -12,8 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let rootVC = MalachiteView()
-        window?.rootViewController = rootVC
+        window?.rootViewController = MalachiteView()
         window?.makeKeyAndVisible()
         
         MalachiteSettingsUtils().ensurePreferences()
