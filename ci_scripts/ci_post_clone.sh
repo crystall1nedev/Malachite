76bcb6804b77523de590de96544fd17634e5068c
Eva Isabella Luna <me@crystall1ne.dev>
2025-05-13 20:00:47 -0600

    Fix Xcode Cloud failing builds

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+#  ci_post_xcodebuild.sh
+#  Malachite
+#
+#  Created by Eva Isabella Lunaon 5/13/25.
+#
+
+mv $CI_PRIMARY_REPOSITORY_PATH/Malachite/Codesigning.example.xcconfig $CI_PRIMARY_REPOSITORY_PATH/Malachite/Codesigning.xcconfig 
