ce0666dede11c74e69c49be946cb071f1c6154d0
Eva Isabella Luna <me@crystall1ne.dev>
2026-03-13 03:07:11 -0600

    [PhotoPreviewView] Fix watermarking and gain map rotation issues
    I spent a few hours on this, I just needed to apply rotations in different
    places. Showed me another bug I need to fix, also switched the font
    to SF Mono from Menlo.

--- mlchtCamera/Views/PhotoPreviewView/PhotoPreviewView.swift
+++ mlchtCamera/Views/PhotoPreviewView/PhotoPreviewView.swift
@@ -213,21 +213,43 @@ class PhotoPreviewView : UIViewController, UIScrollViewDelegate {
      - If ``enableHEIC`` is enabled, create a HEIC representation of all above images combined. Otherwise, JPEG is used.
      */
     public func finalizeImageForExport(imageData: Data) -> Data {
-		guard let rawImage = CIImage(data: imageData, options: [.toneMapHDRtoSDR : (enableHDR ? true : false)]) else { return Data() }
+        guard let rawImage = CIImage(data: imageData, options: [.toneMapHDRtoSDR : (enableHDR ? true : false)]) else { return Data() }
+
+        let exifOrientationKey = kCGImagePropertyOrientation as String
+        let exifOrientationValue = (rawImage.properties[exifOrientationKey] as? NSNumber)?.intValue
+        let orientedCI: CIImage
+        if let exif = exifOrientationValue, let cgOrientation = CGImagePropertyOrientation(rawValue: UInt32(exif)) {
+            orientedCI = rawImage.oriented(cgOrientation)
+        } else {
+            orientedCI = rawImage
+        }
+
+        let context = CIContext()
+        guard let cg = context.createCGImage(orientedCI, from: orientedCI.extent) else { return Data() }
+        let upright = CIImage(cgImage: cg)
+
         var imageProperties = rawImage.properties
-        
-        let watermarkImage = CIImage(image: self.watermark())
-        let outputImage = watermarkImage!.composited(over: rawImage)
-        
+        imageProperties[exifOrientationKey] = nil
+
+        let canvasSize = upright.extent.size
+        let watermarkUIImage = self.watermark(canvasSize: canvasSize)
+        let watermarkImage = CIImage(image: watermarkUIImage)
+
+        let outputImage = (watermarkImage ?? CIImage()).composited(over: upright)
+
         if utilities.preferences.debug.logging.imageProps {
             for prop in imageProperties {
                 MalachiteClassesObject().internalNSLog("[Capture Photo] \(prop)")
             }
         }
-        
+
         let outputImageWithProps = outputImage.settingProperties(imageProperties)
-        
-        return returnImageFile(imageForRepresentation: outputImageWithProps, imageForGainMap: returnGainMap(properties: &imageProperties, imageData: imageData), imageColorspace: rawImage.colorSpace?.name)
+
+        return returnImageFile(
+            imageForRepresentation: outputImageWithProps,
+            imageForGainMap: returnGainMap(properties: &imageProperties, imageData: imageData),
+            imageColorspace: rawImage.colorSpace?.name
+        )
     }
     
     /**
@@ -235,34 +257,46 @@ class PhotoPreviewView : UIViewController, UIScrollViewDelegate {
      
      FIX: A lot of things here, primarly watermark and image rotation being misaligned.
      */
-    func watermark() -> UIImage
-    {
-        let imageView = UIImageView()
-        imageView.backgroundColor = UIColor.clear
-        
-        if photoImage.size.width < photoImage.size.height {
-            imageView.frame = CGRect(x:0, y:0, width:photoImage.size.height, height:photoImage.size.width)
-        } else {
-            imageView.frame = CGRect(x:0, y:0, width:photoImage.size.width, height:photoImage.size.height)
+    func watermark(canvasSize: CGSize, inset: CGPoint = CGPoint(x: 20, y: 20)) -> UIImage {
+        let imageView = UIImageView(frame: CGRect(origin: .zero, size: canvasSize))
+        imageView.backgroundColor = .clear
+
+        guard utilities.preferences.watermark.enabled else {
+            UIGraphicsBeginImageContextWithOptions(canvasSize, false, 1)
+            let img = UIGraphicsGetImageFromCurrentImageContext()!
+            UIGraphicsEndImageContext()
+            return img
         }
+
+        let text = utilities.preferences.watermark.text
+        let font = UIFont.monospacedSystemFont(ofSize: 70, weight: .regular)
+
+        let label = UILabel()
+        label.textAlignment = .right
+        label.textColor = .white
+        label.text = text
+        label.font = font
+        label.backgroundColor = .clear
+        label.numberOfLines = 1
+
+        label.sizeToFit()
         
-        if utilities.preferences.watermark.enabled {
-            utilities.debugNSLog("[Watermarking] User has opted to show a watermark")
-            var label = UILabel()
-            label = UILabel(frame: CGRect(x:50, y:20, width:photoImage.size.width - 100, height:120))
-            label.textAlignment = .left
-            label.textColor = .white
-            label.text = utilities.preferences.watermark.text
-            label.font = UIFont(name: "Menlo", size: 70)
-            
-            imageView.addSubview(label)
-        }
+        let originalSize = label.bounds.size
         
-        UIGraphicsBeginImageContext(imageView.bounds.size)
+        label.frame = CGRect(origin: .zero, size: originalSize)
+        label.transform = CGAffineTransform(rotationAngle: .pi / 2)
+        let finalX = canvasSize.width - inset.x - originalSize.height
+        let finalY = inset.y
+        label.frame.origin = CGPoint(x: finalX, y: finalY)
+
+        imageView.addSubview(label)
+
+        UIGraphicsBeginImageContextWithOptions(canvasSize, false, 1)
         imageView.layer.render(in: UIGraphicsGetCurrentContext()!)
-        let imageWithText = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext();
-        return imageWithText!
+        let imageWithText = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+
+        return imageWithText
     }
     
     /// Function to return a HEIC representation of the passed image  with its colorspace and an optional gain map image.
@@ -286,9 +320,16 @@ class PhotoPreviewView : UIViewController, UIScrollViewDelegate {
     
     /// Function to extract gain map data from the image.
     func returnGainMap(properties props: inout [String: Any], imageData: Data) -> CIImage? {
-		if !enableHDR { return nil }
+        if !enableHDR { return nil }
         var gainMapImage = CIImage()
-        if let gainMapDataInfo = CGImageSourceCopyAuxiliaryDataInfoAtIndex(CGImageSourceCreateWithData(NSData(data: imageData), nil)!, 0, kCGImageAuxiliaryDataTypeHDRGainMap) as? Dictionary<CFString, Any> {
+
+        guard let source = CGImageSourceCreateWithData(NSData(data: imageData), nil) else { return nil }
+
+        let propsDict = CGImageSourceCopyPropertiesAtIndex(source, 0, nil) as? [CFString: Any]
+        let orientationCF = propsDict?[kCGImagePropertyOrientation] as? NSNumber
+        let sourceOrientation = orientationCF.flatMap { CGImagePropertyOrientation(rawValue: $0.uint32Value) }
+
+        if let gainMapDataInfo = CGImageSourceCopyAuxiliaryDataInfoAtIndex(source, 0, kCGImageAuxiliaryDataTypeHDRGainMap) as? Dictionary<CFString, Any> {
             utilities.debugNSLog("[Capture Photo] Saving gain map properties from image")
             let gainMapData = gainMapDataInfo[kCGImageAuxiliaryDataInfoData] as! Data
             let gainMapDescription = gainMapDataInfo[kCGImageAuxiliaryDataInfoDataDescription]! as! [String: Int]
@@ -297,28 +338,37 @@ class PhotoPreviewView : UIViewController, UIScrollViewDelegate {
                                          bytesPerRow: gainMapDescription["BytesPerRow"]!,
                                          size: gainMapSize,
                                          format: .L8, colorSpace: nil)
-            let gainMapcgImage = CIContext().createCGImage(gainMapciImage,
-                                                           from: CGRect(origin: CGPoint(x: 0, y: 0), size: gainMapSize))!
+
+            let orientedGainMap: CIImage
+            if let srcOri = sourceOrientation {
+                orientedGainMap = gainMapciImage.oriented(srcOri)
+            } else {
+                orientedGainMap = gainMapciImage
+            }
+
+            let context = CIContext()
+            let gainMapcgImage = context.createCGImage(orientedGainMap,
+                                                       from: CGRect(origin: CGPoint(x: 0, y: 0), size: orientedGainMap.extent.size))!
             let gainMapOutputData = NSMutableData()
             let gainMapDest = CGImageDestinationCreateWithData(gainMapOutputData, UTType.bmp.identifier as CFString, 1, nil)
             CGImageDestinationAddImage(gainMapDest!, gainMapcgImage, [:] as CFDictionary)
             CGImageDestinationFinalize(gainMapDest!)
-            
+
             gainMapImage = CIImage(data: gainMapOutputData as Data)!
-            
+
             var applDict = extractEXIFData(properties: props, dictionary: kCGImagePropertyMakerAppleDictionary)
             var exifDict = extractEXIFData(properties: props, dictionary: kCGImagePropertyExifDictionary)
-            
+
             applDict["33"] = 0.0
             applDict["48"] = 0.0
             exifDict["CustomRendered"] = 2
-            
+
             props[kCGImagePropertyMakerAppleDictionary as String] = applDict
             props[kCGImagePropertyExifDictionary as String] = exifDict
         } else {
             utilities.debugNSLog("[Capture Photo] Couldn't save the gain map properties. Opting to ignore.")
         }
-        
+
         return gainMapImage
     }
     
@@ -367,3 +417,4 @@ final class dataToShareable: NSObject, UIActivityItemSource {
         return metadata
     }
 }
+
